1121830ea896d05a75b5da2a9880da99e5443830
Rob Prellwitz <robprellwitz@uwalumni.com>
2020-10-25 23:40:07 -0500

    methods to add ships to playerboard

--- BattleShip/Control.cs
+++ BattleShip/Control.cs
@@ -3,24 +3,35 @@ namespace BattleShip
 {
     public class Control
     {
-       public GameBoard testGame;
+       //public GameBoard testGame;
+        public Player player1;
+        public Array tester;
 
         public Control()
         {
             Console.WriteLine("test");
 
-            testGame = new GameBoard();
-
-
-            //testGame.PlayerBoard();
-            //testGame.PrintBoard();
-
+            //testGame = new GameBoard();
+            player1 = new Player();
+            
         }
 
         public void sample()
         {
-            testGame.PlayerBoard();
-            testGame.PrintBoard();
+            
+            tester = player1.gameBoard.PlayerBoard();
+            player1.PositionShip(player1.submarine);
+            player1.CheckPosition(player1.submarine, tester);
+            player1.SetPosition(player1.submarine, tester);
+            player1.gameBoard.PrintBoard(tester);
+            player1.PositionShip(player1.destroyer);
+            player1.CheckPosition(player1.destroyer, tester);
+            player1.SetPosition(player1.destroyer, tester);
+            player1.gameBoard.PrintBoard(tester);
+            player1.PositionShip(player1.carrier);
+            player1.CheckPosition(player1.carrier, tester);
+            player1.SetPosition(player1.carrier, tester);
+            player1.gameBoard.PrintBoard(tester);
         }
 
 
--- BattleShip/Player.cs
+++ BattleShip/Player.cs
@@ -1,36 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Security.Cryptography.X509Certificates;
 
 namespace BattleShip
 {
-    public abstract class Player
+    public class Player
     {
+        // member Variables 
+        int x; int y;
         public string name;
         public int ships;
         public List<Ship> fleet;
-       
+        public GameBoard gameBoard;
+       // public GameBoard opponentBoard;
+        public Submarine submarine;
+        public BattleShip battleShip;
+        public AirCraftCarrier carrier;
+        public Destroyer destroyer;
+        
         public Player()
-        {
+        { // Constructor
+            
+            gameBoard = new GameBoard();
+            
+            submarine = new Submarine();
+            destroyer = new Destroyer();
+
+           
+            
+
         }
+
+        
+
+        public void PositionShip(Ship ship)
+        { // 1 = horizontal 2 = vertical
+           
+            {
+                for (int i = 0; i < ship.size; i++)
+                {
+                    int choicex;
+                    int choicey;
+                    Console.WriteLine("\nInput the x Coordinate:\n");
+                    bool checkx = Int32.TryParse(Console.ReadLine(), out choicex);
+                    Console.WriteLine("Input the y Coordinate:\n");
+                    bool checky = Int32.TryParse(Console.ReadLine(), out choicey);
+                    ship.position.Add((choicex, choicey));
+                }
+            }
+
+        }
+
+        public bool CheckPosition(Ship ship, Array gameBoard)
+        {
+            bool check = true;
+            int i = 0;
+            while (i < ship.size) 
+            {
+                (int x, int y) = ship.position[i];
+
+                if ((char)gameBoard.GetValue(x, y) != 'O') ;
+                {
+                        check = false;
+                        break;
+                }
+                i++;
+            }
+            return check;
+
+        }
+        public void SetPosition(Ship ship, Array gameBoard)
+        {
+            for (int i = 0; i < ship.size; i++)
+            {
+                (int x, int y) = ship.position[i];
+                gameBoard.SetValue('S', x, y);
+            }
+        }
+
+
 
     }
 
-    public class Player1 : Player
-    {
-        public Player1()
-        {
-            name = "Player 1";
-            ships = 5;
-        }
-    }
-
-    public class Player2 : Player
-    {
-        public Player2()
-        {
-            name = "Player 2";
-            ships = 5;
-        }
-    }
+   
 }
